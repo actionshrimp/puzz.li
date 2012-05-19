8637f57aa56aeb4707e5b85a21140f644231c0fe
Dave Aitken <dave.aitken@gmail.com>
2012-05-19 22:02:06 +0100

    Initial commit

---
+++ boot.js
@@ -0,0 +1,4 @@
+var server = require('./');
+
+server.listen(3000);
+console.log("Express server listening on port %d in %s mode", server.address().port, server.settings.env);
---
+++ index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var assets= require('connect-assets');
+
+var app = module.exports = express.createServer();
+
+app.configure(function() {
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(assets());
+    app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
+
+app.configure('production', function(){
+    app.use(express.errorHandler()); 
+});
+
+//Set up basic routing
+require('./routes.js')(app);
+
+//Include express-resource for REST
+//require('express-resource');
+//require('./resources')(app);
+
