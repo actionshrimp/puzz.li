9893d82e84684b55a78b88ce4d65d191c78a6b0e
Dave Aitken <dave.aitken@gmail.com>
2012-08-13 22:27:11 +0100

    Simple sudoku page layout and stylings

--- routes.js
+++ routes.js
@@ -3,4 +3,8 @@ module.exports = function(app) {
     app.get('/', function(req, res) {
         res.render('index', {title: 'Puzzli'});
     });
+
+    app.get('/sudoku', function(req, res) {
+        res.render('puzzle', {title: 'Sudoku'});
+    })
 };
